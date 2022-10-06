3db944953115039d97731c4fffe83c62b62a88ef
Gianfranco Raselli <66041020+GianfrancoRaselli@users.noreply.github.com>
2022-10-06 18:09:55 -0300

    fix img routes

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -63,7 +63,7 @@
             <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="wrap-item text-center">
                 <div class="item-img">
-                  <img src="img/ser01.png" />
+                  <img src="@/assets/img/ser01.png" />
                 </div>
                 <h3 class="pad-bt15">Creative Concept</h3>
                 <p>
@@ -75,7 +75,7 @@
             <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="wrap-item text-center">
                 <div class="item-img">
-                  <img src="img/ser02.png" />
+                  <img src="@/assets/img/ser02.png" />
                 </div>
                 <h3 class="pad-bt15">Amazing Design</h3>
                 <p>
@@ -87,7 +87,7 @@
             <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="wrap-item text-center">
                 <div class="item-img">
-                  <img src="img/ser03.png" />
+                  <img src="@/assets/img/ser03.png" />
                 </div>
                 <h3 class="pad-bt15">Cost effective</h3>
                 <p>
@@ -99,7 +99,7 @@
             <div class="col-md-3 col-sm-6 col-xs-12">
               <div class="wrap-item text-center">
                 <div class="item-img">
-                  <img src="img/ser04.png" />
+                  <img src="@/assets/img/ser04.png" />
                 </div>
                 <h3 class="pad-bt15">Secure</h3>
                 <p>
@@ -184,7 +184,7 @@
             </div>
             <div class="col-md-4 col-sm-6 col-xs-12 portfolio-item padding-right-zero mr-btn-15">
               <figure>
-                <img src="img/port01.jpg" class="img-responsive" />
+                <img src="@/assets/img/port01.jpg" class="img-responsive" />
                 <figcaption>
                   <h2>Project For Everyone</h2>
                   <p>
@@ -196,7 +196,7 @@
             </div>
             <div class="col-md-4 col-sm-6 col-xs-12 portfolio-item padding-right-zero mr-btn-15">
               <figure>
-                <img src="img/port02.jpg" class="img-responsive" />
+                <img src="@/assets/img/port02.jpg" class="img-responsive" />
                 <figcaption>
                   <h2>Project For Everyone</h2>
                   <p>
@@ -208,7 +208,7 @@
             </div>
             <div class="col-md-4 col-sm-6 col-xs-12 portfolio-item padding-right-zero mr-btn-15">
               <figure>
-                <img src="img/port03.jpg" class="img-responsive" />
+                <img src="@/assets/img/port03.jpg" class="img-responsive" />
                 <figcaption>
                   <h2>Project For Everyone</h2>
                   <p>
@@ -220,7 +220,7 @@
             </div>
             <div class="col-md-4 col-sm-6 col-xs-12 portfolio-item padding-right-zero mr-btn-15">
               <figure>
-                <img src="img/port04.jpg" class="img-responsive" />
+                <img src="@/assets/img/port04.jpg" class="img-responsive" />
                 <figcaption>
                   <h2>Project For Everyone</h2>
                   <p>
@@ -232,7 +232,7 @@
             </div>
             <div class="col-md-4 col-sm-6 col-xs-12 portfolio-item padding-right-zero mr-btn-15">
               <figure>
-                <img src="img/port05.jpg" class="img-responsive" />
+                <img src="@/assets/img/port05.jpg" class="img-responsive" />
                 <figcaption>
                   <h2>Project For Everyone</h2>
                   <p>
@@ -244,7 +244,7 @@
             </div>
             <div class="col-md-4 col-sm-6 col-xs-12 portfolio-item padding-right-zero mr-btn-15">
               <figure>
-                <img src="img/port06.jpg" class="img-responsive" />
+                <img src="@/assets/img/port06.jpg" class="img-responsive" />
                 <figcaption>
                   <h2>Project For Everyone</h2>
                   <p>
@@ -267,7 +267,7 @@
                 <ul class="bxslider">
                   <li>
                     <blockquote>
-                      <img src="img/thumb.png" class="img-responsive" />
+                      <img src="@/assets/img/thumb.png" class="img-responsive" />
                       <p>
                         Come a day there won't be room for naughty men like us to slip about at all. This job goes south, there
                         well may not be another.
@@ -277,14 +277,14 @@
                   </li>
                   <li>
                     <blockquote>
-                      <img src="img/thumb.png" class="img-responsive" />
+                      <img src="@/assets/img/thumb.png" class="img-responsive" />
                       <p>So here is us, on the raggedy edge. Don't push me, and I won't push you.</p>
                     </blockquote>
                     <small>Marry Smith, Client</small>
                   </li>
                   <li>
                     <blockquote>
-                      <img src="img/thumb.png" class="img-responsive" />
+                      <img src="@/assets/img/thumb.png" class="img-responsive" />
                       <p>
                         Come a day there won't be room for naughty men like us to slip about at all. This job goes south, there
                         well may not be another.
@@ -294,7 +294,7 @@
                   </li>
                   <li>
                     <blockquote>
-                      <img src="img/thumb.png" class="img-responsive" />
+                      <img src="@/assets/img/thumb.png" class="img-responsive" />
                       <p>So here is us, on the raggedy edge. Don't push me, and I won't push you.</p>
                     </blockquote>
                     <small>John Doe, Client</small>
@@ -321,7 +321,7 @@
               <div class="blog-sec">
                 <div class="blog-img">
                   <a href="">
-                    <img src="img/blog01.jpg" class="img-responsive" />
+                    <img src="@/assets/img/blog01.jpg" class="img-responsive" />
                   </a>
                 </div>
                 <div class="blog-info">
@@ -349,7 +349,7 @@
               <div class="blog-sec">
                 <div class="blog-img">
                   <a href="">
-                    <img src="img/blog02.jpg" class="img-responsive" />
+                    <img src="@/assets/img/blog02.jpg" class="img-responsive" />
                   </a>
                 </div>
                 <div class="blog-info">
@@ -377,7 +377,7 @@
               <div class="blog-sec">
                 <div class="blog-img">
                   <a href="">
-                    <img src="img/blog03.jpg" class="img-responsive" />
+                    <img src="@/assets/img/blog03.jpg" class="img-responsive" />
                   </a>
                 </div>
                 <div class="blog-info">
